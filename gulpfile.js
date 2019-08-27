74f9827572b44e25e45a40d5a3cb155662b7f495
Aleksandar Kamenov <aleksandyr.kamenov@gmail.com>
2019-08-27 11:47:41 +0300

    change typedoc theme structure and fix css sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,8 @@ const shell = require('gulp-shell');
 
 
 const TYPEDOC_THEME = {
-    SRC: slash(path.join(__dirname, 'typedoc', 'src')),
-    DIST: slash(path.join(__dirname, 'typedoc','bin')),
+    SRC: slash(path.join(__dirname, 'src')),
+    DIST: slash(path.join(__dirname, 'bin')),
     STYLES: {
         ENTRY: slash(path.join('assets', 'css', 'main.sass')),
         OUT: slash(path.join('assets','css')),
@@ -68,7 +68,7 @@ const typedocCopyStyles = () => {
     .pipe(sourcemaps.init())
     .pipe(sass.sync(TYPEDOC_THEME.STYLES.CONFIG).on('error', sass.logError))
     .pipe(prefixer)
-    .pipe(sourcemaps.write(TYPEDOC_THEME.STYLES.MAPS))
+    .pipe(sourcemaps.write('./'))
     .pipe(dest(slash(path.join(TYPEDOC_THEME.DIST, TYPEDOC_THEME.STYLES.OUT))));
 };
 typedocCopyStyles.displayName = 'typedoc:copy-styles';
@@ -108,7 +108,7 @@ const typedocCleanJS = async () => {
 typedocCleanJS.displayName = 'typedoc:clean-js';
 
 const typedocBuildTS = async () => {
-  const buildTs = shell.task(`tsc --project ${path.join(__dirname, 'typedoc', 'tsconfig.json')}`);
+  const buildTs = shell.task(`tsc --project ${path.join(__dirname, 'tsconfig.json')}`);
   return await buildTs();
 };
 typedocBuildTS.displayName = 'typedoc:build-ts';
